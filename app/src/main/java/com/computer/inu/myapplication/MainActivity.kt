52a286ec7007f5e3eb8faa49b31c7fee48e66778
KimMooHyeon <dlfb77@gmail.com>
2019-04-13 20:52:29 +0900

    change Main View image to Viewpager with indicator but not finish

--- app/src/main/java/com/computer/inu/myapplication/MainActivity.kt
+++ app/src/main/java/com/computer/inu/myapplication/MainActivity.kt
@@ -2,14 +2,33 @@ package com.computer.inu.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
+import android.graphics.drawable.Drawable
+import android.support.design.widget.TabLayout
+import android.support.v4.view.ViewPager
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val temp: List<Drawable>
+        temp = ArrayList()
+        temp.add(ContextCompat.getDrawable(this,R.drawable.monnjaein)!!);
+        temp.add(ContextCompat.getDrawable(this,R.drawable.hongjunpyo)!!);
+        temp.add(ContextCompat.getDrawable(this,R.drawable.youseounmin)!!);
+
+       var a =  Adapter(temp,this);
+
+       var pager = findViewById(R.id.photos_viewpager) as ViewPager
+        pager.setAdapter(a);
+
+
+      var tabLayout = findViewById(R.id.tab_layout) as TabLayout
+        tabLayout.setupWithViewPager(pager, true);
 
 
     }
